1ae6991386a9a7370a8b16015cbca0973e7d45d4
Jafea7 <73450040+Jafea7@users.noreply.github.com>
2021-08-07 16:30:17 +1000

    @Datahell integrated changes

--- RecFilter3.py
+++ RecFilter3.py
@@ -40,8 +40,10 @@ signal.signal(signal.SIGINT, exit_handler) # Handle Ctrl-C
 print('\n--- RecFilter2 ---')
 parser = argparse.ArgumentParser(prog='RecFilter', description='RecFilter: Remove SFW sections of videos')
 parser.add_argument('file', type=str, help='Video file to process')
-parser.add_argument('-i', '--interval', type=int, default=60, help='Interval between image samples (default: 60)')
-parser.add_argument('-e', '--extension', type=int, default=0, help='Extend time prior to section start (default: 0)')
+parser.add_argument('-i', '--interval', type=int, default=20, help='Interval between image samples (default: 20)')
+parser.add_argument('-c', '--cut', type=int, default=30, help='Trigger a cut when x seconds without match (default: 30)')
+parser.add_argument('-d', '--duration', type=int, default=20, help='Minimum duration of segments (default: 20)')
+parser.add_argument('-e', '--extension', type=int, default=5, help='Extend start and end of segments by x seconds (default: 5)')
 parser.add_argument('-b', '--beginning', type=int, default=1, help='Skip x seconds of beginning (default: 1)')
 parser.add_argument('-f', '--finish', type=int, default=0, help='Skip x seconds of finish (default: 0)')
 parser.add_argument('-m', '--model', type=str, help='Model name for config preset')
@@ -50,22 +52,23 @@ parser.add_argument('-k', '--keep', action='store_true', help='Keep temporary wo
 parser.add_argument('-v', '--verbose', action='store_true', help='Output working information (default: False)')
 
 args = parser.parse_args()
-if ((((args.model is not None) and 
-    (args.site is None))) or
-    ((args.site is not None) and
-    (args.model is None))):
-  parser.error('The -model argument requires a -site argument')
+if ((args.site is not None) and 
+    (args.model is None)):
+  parser.error('The --site argument requires a --model argument')
 
 video_name = args.file
-frame_duration = args.interval
-frame_extension = args.extension
+sample_interval = args.interval
+min_segment_duration = args.duration
+cut_trigger = args.cut
+segment_extension = args.extension
 skip_begin = args.beginning
 skip_finish = args.finish
 if args.model is not None:
   model = args.model.lower()
 else:
   model = None
-if args.site is not None:
+if ((args.site is not None) and
+    (model is not None)):
   site = args.site.lower()
 else:
   site = None
@@ -89,22 +92,29 @@ if config:
   if 'default' in data:
     if str(data['default']) != "":
       checkinglist = data['default'].split(',')
-  if ((model is not None) and (site is not None)):
+  if 'videoext' in data:
+    if str(data['videoext']) != "":
+      fileext = data['videoext']
+    else:
+      fileext = "mp4"
+  if ((model is not None)):
     found = False
     for cammodel in data['models']:
       if (cammodel['name'].lower() == model):
-        if (cammodel['site'].lower() == site):
-          frame_duration = cammodel['interval']
-          frame_extension = cammodel['extension']
-          checkinglist = cammodel['search'].split(',')
-          skip_begin = cammodel['begin']
-          skip_finish = cammodel['finish']
-          found = True
-          break
+#        if (cammodel['site'].lower() == site):
+        sample_interval = cammodel['interval']
+        cut_trigger = cammodel['cut']
+        min_segment_duration = cammodel['duration']
+        segment_extension = cammodel['extension']
+        checkinglist = cammodel['search'].split(',')
+        skip_begin = cammodel['begin']
+        skip_finish = cammodel['finish']
+        found = True
+        break
     if not found:
-      print('INFO: \'' + model + '\' at \'' + site + '\' not found, using defaults.')
+      print('INFO: \'' + model + '\' not found, using defaults.')
 
-print('INFO: -i ' + str(frame_duration) + ' -e ' + str(frame_extension) + ' -b ' + str(skip_begin) + ' -f ' + str(skip_finish))
+print('INFO: -i ' + str(sample_interval) + ' -c ' + str(cut_trigger) + ' -d ' + str(min_segment_duration)+ ' -e ' + str(segment_extension) + ' -b ' + str(skip_begin) + ' -f ' + str(skip_finish))
 print('      ' + str(checkinglist))
 
 if checkinglist[0] == 'NONE':
@@ -137,15 +147,15 @@ duration = int(float(open('tmp', 'r').read().strip())) - skip_finish
 if verbose: print('Duration: ' + str(duration))
 
 print('INFO: Creating sample images')
-for interval in range(skip_begin, duration, frame_duration):
-  os.system('ffmpeg -v quiet -y -skip_frame nokey -ss ' + str(interval) + ' -i "' + video_path + '" -vf select="eq(pict_type\\,I),scale=800:-1" -an -q:v 3 -vframes 1 image-' + str(interval).zfill(7) + '.bmp')
+for interval in range(skip_begin, duration, sample_interval):
+  os.system('ffmpeg -v quiet -y -skip_frame nokey -ss ' + str(interval) + ' -i "' + video_path + '" -vf select="eq(pict_type\\,I),scale=800:-1" -an -q:v 3 -vframes 1 image-' + str(interval).zfill(7) + '.png')
 if skip_finish < 1:
-  os.system('ffmpeg -v quiet -y -skip_frame nokey -ss ' + str(duration - 1) + ' -i "' + video_path + '" -vf select="eq(pict_type\\,I),scale=800:-1" -an -q:v 3 -vframes 1 image-' + str(duration - 1).zfill(7) + '.bmp')
+  os.system('ffmpeg -v quiet -y -skip_frame nokey -ss ' + str(duration - 1) + ' -i "' + video_path + '" -vf select="eq(pict_type\\,I),scale=800:-1" -an -q:v 3 -vframes 1 image-' + str(duration - 1).zfill(7) + '.png')
 
 print('INFO: Analysing images')
 with open('API-Results.txt',"w") as outfile:
   for filename in os.listdir(os.getcwd()):
-    if filename.endswith(".bmp"):
+    if filename.endswith(".png"):
       output  = detector.detect(filename)
       y = 0
       stringoutput = ''
@@ -165,65 +175,65 @@ with open('API-Results.txt',"r") as infile, open('output.txt',"w") as outfile:
         outfile.write(line)
         break
 
-
 with open('output.txt',"r") as infile, open('list.txt',"w") as outfile:
   for line in infile:
     match = re.search(r'\d\d\d\d\d\d\d', line)
     x = int(match.group())
     imagelist.append(x)
     lines.append(line)
-  
+
   while i < len(imagelist):
-    if i == 0: #if first element of imagelist
-      if len(imagelist) == 1 or imagelist[i + 1] - imagelist[i] > frame_duration: #if no segment within frame_durations
-        beginnings.append(imagelist[i]) 
-        endings.append(imagelist[i] + frame_duration) #become your own frame_durations segment
-      else: 
-        b = imagelist[i] #else become beginning of multi-minute-segment
-        if e > b:
-          beginnings.append(b)
-          endings.append(e)
+# variables
+    last_element = len(imagelist) - 1
+#avoid using elements not existent in list
+    if i == 0: gap_to_prev_match = imagelist[i]
+    else: gap_to_prev_match = imagelist[i] - imagelist[i - 1]
+    if i == last_element: gap_to_next_match = imagelist[i]
+    else: gap_to_next_match = imagelist[i + 1] - imagelist[i]
+#added time between two samples if they were split apart by 1 second
+    cut_duration = cut_trigger + 2 * segment_extension + 1
+    segment_start = imagelist[i] - segment_extension
+    segment_end = imagelist[i] + segment_extension
+    
+# case for finding the start of a segment
+# if first element has matches in reach become beginning
+# if previous match is too far away
+    if (i == 0 and gap_to_next_match <= cut_duration) or (gap_to_prev_match > cut_duration):
+      #save beginning timestamp
+      if segment_start > 0: b = segment_start #only include segment_extension if timestamp doesn't become negative
+      else: b = 0
 
-    elif i == len(imagelist) - 1: #if last element if imagelist
-      if imagelist[i] - imagelist[i - 1] > frame_duration: #if no segment within frame_durations
-        beginnings.append(imagelist[i] - frame_extension) 
-        endings.append(imagelist[i] + frame_duration) #become your own frame_durations segment
-      else:
-        e = imagelist[i] #else become ending of multi-minute-segment
-        if e > b:
-          beginnings.append(b)
-          endings.append(e)
-
-    elif imagelist[i + 1] - imagelist[i] > frame_duration and imagelist[i] - imagelist[i - 1] > frame_duration: #lone wolf segment example: if model flashes breasts on contactsheet frame
-      beginnings.append(imagelist[i] - frame_extension) 
-      endings.append(imagelist[i] + frame_duration) #become your own frame_durations segment
-
-    elif imagelist[i + 1] - imagelist[i] > frame_duration: #multi-minute ending segment
-      e = imagelist[i] + frame_duration
-      if e > b:
+# case for finding the end of a segment and finalizing it
+# if next match is too far away
+# if last element while previous match is close
+    if (gap_to_next_match > cut_duration) or (i == last_element and gap_to_prev_match <= cut_duration):
+      #save ending timestamp, only include segment_extension if timestamp doesn't exceed file duration
+      if duration > segment_end: e = segment_end
+      else: e = duration
+      segment_duration = e - b
+# finalize segment
+# only finalize if the result would have non negative duration
+# only finalize segment if long enough
+      if (segment_duration >= 0) and (segment_duration >= min_segment_duration):
         beginnings.append(b)
         endings.append(e)
 
-    elif imagelist[i] - imagelist[i - 1] > frame_duration: #multi-minute beginning segment
-      b = imagelist[i] - frame_extension
-      if e > b:
-        beginnings.append(b)
-        endings.append(e)
+# else go to next sample without doing anything
     i += 1
-      
+
   if verbose: print('Image list: ' + str(imagelist) + '\nBeginnings: ' + str(beginnings) + '\nEndings: ' + str(endings))
 
   print('INFO: Creating video segments')
   while p < len(beginnings):
     duration = endings[p] - beginnings[p]
-    outfile.write('file ' + '\'out' + str(p) + '.mp4\'' + '\n')
-    if verbose: print('ffmpeg -v quiet -vsync 0 -ss ' + str(beginnings[p]) + ' -i "' + video_path + '" -t ' + str(duration) + ' -c copy out' + str(p) + '.mp4')
-    os.system('ffmpeg -v quiet -vsync 0 -ss ' + str(beginnings[p]) + ' -i "' + video_path + '" -t ' + str(duration) + ' -c copy out' + str(p) + '.mp4')
+    outfile.write('file ' + '\'out' + str(p) + '.mkv\'' + '\n')
+    if verbose: print('ffmpeg -v quiet -vsync 0 -ss ' + str(beginnings[p]) + ' -i "' + video_path + '" -t ' + str(duration) + ' -c copy out' + str(p) + '.' + str(fileext))
+    os.system('ffmpeg -v quiet -vsync 0 -ss ' + str(beginnings[p]) + ' -i "' + video_path + '" -t ' + str(duration) + ' -c copy out' + str(p) + '.' + str(fileext))
     p += 1
 
 print('INFO: Creating final video')
-if verbose: print('ffmpeg -v quiet -y -vsync 0 -safe 0 -f concat -i list.txt -c copy "' + video_path.rsplit('.', 1)[0] + '-Compilation' + str(frame_duration) + '-' + str(frame_extension) + '.mp4"')
-os.system('ffmpeg -v quiet -y -vsync 0 -safe 0 -f concat -i list.txt -c copy "' + video_path.rsplit('.', 1)[0] + '-Compilation' + str(frame_duration) + '-' + str(frame_extension) + '.mp4"')
+if verbose: print('ffmpeg -v quiet -y -vsync 0 -safe 0 -f concat -i list.txt -c copy "' + video_path.rsplit('.', 1)[0] + '_recfilter-i' + str(sample_interval) + '-c' + str(cut_trigger) + '-d' + str(min_segment_duration) + '-e' + str(segment_extension) + '.' + str(fileext) + '"')
+os.system('ffmpeg -v quiet -y -vsync 0 -safe 0 -f concat -i list.txt -c copy "' + video_path.rsplit('.', 1)[0] + '_recfilter-i' + str(sample_interval) + '-c' + str(cut_trigger) + '-d' + str(min_segment_duration) + '-e' + str(segment_extension) + '.' + str(fileext) + '"')
 
 popdir() # Return to temporary directory parent
 if (not keep): # Delete the temporary directory if no -keep
