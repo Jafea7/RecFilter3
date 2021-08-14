e6cc40f0ce097a70fe5792cbdd1379479378044b
Datah3ll <66582666+Datah3ll@users.noreply.github.com>
2021-08-14 16:57:48 +0200

    Update RecFilter3.py

--- RecFilter3.py
+++ RecFilter3.py
@@ -348,7 +348,7 @@ if 4 in code_sections: #on/off switch for code
       cut_duration = segment_gap + 2 * segment_extension + 1
       segment_start = imagelist[i] - segment_extension
       segment_end = imagelist[i] + segment_extension
-      
+
 # case for finding the start of a segment
       #if first element has matches in reach become beginning
       #or if previous match is too far away
@@ -357,7 +357,7 @@ if 4 in code_sections: #on/off switch for code
         if segment_start >= 0: b = segment_start #segment_extension only if timestamp doesn't become negative
         else: 
           b = 0 #otherwise use 0 as a beginning
-          found_segment_start = True
+        found_segment_start = True
 # case for finding the end of a segment and finalizing it
       #if next match is too far away
       #if last element while previous match is close
@@ -365,7 +365,7 @@ if 4 in code_sections: #on/off switch for code
         #save ending timestamp, only include segment_extension if timestamp doesn't exceed file duration
         if duration > segment_end: e = segment_end
         else: e = duration
-        if found_segment_start: #Initialize b as None, since 0 would always make the first cut start at 0
+        if found_segment_start:
           segment_duration = e - b
           #finalize segment
           #only finalize if the result would have a positive duration
