eebdfffb6931b3c5b187dc313b097f5dfa275296
Jafea7 <73450040+Jafea7@users.noreply.github.com>
2021-08-15 17:31:46 +1000

    Update RecFilter3.py

--- RecFilter3.py
+++ RecFilter3.py
@@ -14,9 +14,9 @@ from csv import reader
 
 detector = NudeDetector()
 
-#MIN_PYTHON = (3, 7, 9)
-#if sys.version_info < MIN_PYTHON:
-#    sys.exit("\nPython %s.%s.%s or later is required.\n" % MIN_PYTHON)
+MIN_PYTHON = (3, 7, 6)
+if sys.version_info < MIN_PYTHON:
+  sys.exit("\nPython %s.%s.%s or later is required.\n" % MIN_PYTHON)
 
 pushstack = []
 def pushdir(dirname):
